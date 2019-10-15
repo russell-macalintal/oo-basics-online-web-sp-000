3c236ef073dd215372fc1626561dd056f16c0abe
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-15 03:21:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
 # Make your shoe class here!
+class shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand_name)
+    @brand = brand_name
+  end
+  
+end
